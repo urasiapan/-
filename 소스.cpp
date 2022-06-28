a279654887654039f62e08c0dcc252243fa1c7e8
urasiapan <urasiapan@naver.com>
2022-06-28 21:55:36 +0900

    binary search

--- 소스.cpp
+++ 소스.cpp
@@ -1,32 +1,47 @@
 #include <iostream>
 using namespace std;
+#include <algorithm>
+
+void binarysearch(const int array[], const int& n, const int& k) {
+	int first = 0;
+	int last = n - 1;
+	while (first <= last) {
+		int mid = (first + last) / 2;
+		if (array[mid] == k) {
+			cout << "1\n";
+			return;
+		}
+		else {
+			if (array[mid] > k)
+				last = mid - 1;
+			else
+				first = mid + 1;
+		}
+	}
+	cout << "0\n";
+	return;
+}
 
 int main(void) {
 	ios_base::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	
-	int n, sum;
+
+	int array[500001];
+	int array2[500001];
+
+	int n, m;
 	cin >> n;
-	int result = 0;
-
-	for (int i = 1; i < 1000001; i++) {
-
-		sum = i;
-		int j = i;
-
-		while (j != 0) {
-			sum += j % 10;
-			j /= 10;
-		}
-
-		if (sum == n) {
-			result = i;
-			break;
-		}
+	for (int i = 0; i < n; i++) {
+		cin >> array[i];
 	}
-
-	cout << result;
-
+	cin >> m;
+	for (int i = 0; i < m; i++) {
+		cin >> array2[i];
+	}
+	sort(array, array + n);
+	for (int i = 0; i < m; i++) {
+		binarysearch(array, n, array2[i]);
+	}
 	return 0;
 }
